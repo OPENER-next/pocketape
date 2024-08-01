ea23519bd4f4b0c9506e94f33c762129ccd98147
Robbendebiene <robbendebiene@mailbox.org>
2024-08-01 12:13:28 +0200

    Add convenience API method Simply example based on new convenience method

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,132 +6,95 @@ import 'package:vector_math/vector_math_64.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: MeasurementScreen(),
     );
   }
 }
 
 class MeasurementScreen extends StatefulWidget {
+  const MeasurementScreen({super.key});
+
   @override
-  _MeasurementScreenState createState() => _MeasurementScreenState();
+  State<MeasurementScreen> createState() => _MeasurementScreenState();
 }
 
 class _MeasurementScreenState extends State<MeasurementScreen> {
-  Vector3? _firstValue;
-  Vector3? _lastValue;
-  StreamSubscription<Vector3>? _subscription= null;
-  bool get isMeasuring => _subscription != null;
-
-  void _toggleMeasurement() {
-    if (isMeasuring) {
-      _stopMeasurement();
-    } else {
-      _firstValue = null;
-      _lastValue = null;
-      _startMeasurement();
-    }
-  }
-
-  void _startMeasurement() {
-    if (!isMeasuring) {
-      _subscription = Pocketape.trace().listen(
-        (position) {
-          setState(() {
-            if (_firstValue == null) {
-              _firstValue = position;
-            }
-            else {
-              _lastValue = position;
-            }
-          });
-        },
-        onError: (error) {},
-        onDone: () {
-        },
-      );
-    }
-  }
-
-  String _adjustMeasurement() {
-    var difX = (_lastValue!.x - _firstValue!.x).abs();
-    var difZ = (_lastValue!.z - _firstValue!.z).abs();
-    var difY = (_lastValue!.y - _firstValue!.y).abs();
-    if (difY > difZ && difY > difX) {
-      return "Vertical measure: ${difY.toStringAsFixed(2)}";
-    }
-    else{ 
-      var newDistance = difZ < difX ? difX : difZ;
-      return "Horizontal measure: ${newDistance.toStringAsFixed(2)}";
-    }
-  }
-
-  void _stopMeasurement() {
-    if (isMeasuring) {
-      _subscription!.cancel();
-      setState(() {
-        _subscription = null;
-      });
-    }
-  }
+  Stream<({Vector3 from, Vector3 to})>? _stream;
+  bool get isMeasuring => _stream != null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ARKit Measurement')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('First Value:'),
-            const SizedBox(height: 16),
-            Text(
-              'X: ${_firstValue?.x.toStringAsFixed(2)}, '
-              'Y: ${_firstValue?.y.toStringAsFixed(2)},'
-              'Z: ${_firstValue?.z.toStringAsFixed(2)}',
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 32),
-            const Text('Latest Value:'),
-            const SizedBox(height: 16),
-            Text(
-              'X: ${_lastValue?.x.toStringAsFixed(2)}, '
-              'Y: ${_lastValue?.y.toStringAsFixed(2)}, '
-              'Z: ${_lastValue?.z.toStringAsFixed(2)}',
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: _toggleMeasurement,
-              child: Text(isMeasuring ? 'Stop' : 'Start'),
-            ),
-            const SizedBox(height: 32),
-            Text('Free Distance: ${(_firstValue != null && _lastValue != null)
-              ? _firstValue!.distanceTo(_lastValue!).toStringAsFixed(2)
-              : '0.00'} meters'),
-            const SizedBox(height: 32),
-            Text('Coordinate Replacement: \n${(_firstValue != null && _lastValue != null)
-              ? _adjustMeasurement()
-              : ''}'
-            ),
-          ],
+        child: StreamBuilder<({Vector3 from, Vector3 to})>(
+          initialData: (from: Vector3.zero(), to: Vector3.zero()),
+          stream: _stream,
+          builder: (context, snapshot) {
+            final from = snapshot.requireData.from;
+            final to = snapshot.requireData.to;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('First Value:'),
+                const SizedBox(height: 16),
+                Text(
+                  'X: ${from.x.toStringAsFixed(2)}, '
+                  'Y: ${from.y.toStringAsFixed(2)},'
+                  'Z: ${from.z.toStringAsFixed(2)}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(height: 32),
+                const Text('Latest Value:'),
+                const SizedBox(height: 16),
+                Text(
+                  'X: ${to.x.toStringAsFixed(2)}, '
+                  'Y: ${to.y.toStringAsFixed(2)}, '
+                  'Z: ${to.z.toStringAsFixed(2)}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: _toggleMeasurement,
+                  child: Text(isMeasuring ? 'Stop' : 'Start'),
+                ),
+                const SizedBox(height: 32),
+                Text('Free Distance: ${from.distanceTo(to).toStringAsFixed(2)} meters'),
+                const SizedBox(height: 32),
+                Text( _adjustMeasurement(from, to)),
+              ],
+            );
+          },
         ),
       ),
     );
   }
 
-  @override
-  void dispose() {
-    if (isMeasuring) {
-      _subscription!.cancel();
-      _subscription = null;
+  void _toggleMeasurement() {
+    setState(() {
+      _stream = isMeasuring ? null : Pocketape.traceRange();
+    });
+  }
+
+  String _adjustMeasurement(Vector3 first, Vector3 last) {
+    var difX = (last.x - first.x).abs();
+    var difZ = (last.z - first.z).abs();
+    var difY = (last.y - first.y).abs();
+    if (difY > difZ && difY > difX) {
+      return "Vertical measure: ${difY.toStringAsFixed(2)}";
     }
-    super.dispose();
+    else{
+      var newDistance = difZ < difX ? difX : difZ;
+      return "Horizontal measure: ${newDistance.toStringAsFixed(2)}";
+    }
   }
 }

ea2880bf48aa9dd72fd3dd654d260dd3801b9210
Robbendebiene <robbendebiene@mailbox.org>
2024-08-13 16:21:50 +0200

    Disable platform channel event buffer

--- android/src/main/kotlin/com/openernext/pocketape/PocketapePlugin.kt
+++ android/src/main/kotlin/com/openernext/pocketape/PocketapePlugin.kt
@@ -3,6 +3,7 @@ package com.openernext.pocketape
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.EventChannel.*
 
@@ -14,6 +15,17 @@ class PocketapePlugin : FlutterPlugin, StreamHandler {
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         val messenger = flutterPluginBinding.binaryMessenger
+
+        // The _DefaultBinaryMessenger does buffer 1 event, even when no listener is registered.
+        // This causes the problem of a race condition when an event was or is added to the buffer while the stream is being canceled:
+        // eventBuffer = [EventX];
+        // myStream.cancel();
+        // myStream.listen(...); -> returns EventX
+        //
+        // Therefore we disable buffering by setting it to 0
+        // https://api.flutter.dev/flutter/dart-ui/ChannelBuffers-class.html
+        MethodChannel(messenger, "ar_events").resizeChannelBuffer(0);
+
         eventChannel = EventChannel(messenger, "ar_events")
         arCoreManager = ARCoreManager(flutterPluginBinding.applicationContext, this)
         eventChannel.setStreamHandler(this)
@@ -36,6 +48,5 @@ class PocketapePlugin : FlutterPlugin, StreamHandler {
     override fun onCancel(arguments: Any?) {
         eventSink = null
         arCoreManager?.stopSession()
-        eventSink?.endOfStream()
     }
 }
